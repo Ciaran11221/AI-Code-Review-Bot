76c0db0626c86d47f1cc32ddf7f5b91fb897357d
Your Name <your@email.com>
2026-05-31 21:59:44 +0100

    chore: remove unrelated project files

--- bot-update/bot-update/electron/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-contextBridge.exposeInMainWorld('electron', {
-  getApiKey:    ()      => ipcRenderer.invoke('get-api-key'),
-  saveApiKey:   (key)   => ipcRenderer.invoke('save-api-key', key),
-  deleteApiKey: ()      => ipcRenderer.invoke('delete-api-key'),
-  startBackend: (key)   => ipcRenderer.invoke('start-backend', key),
-  checkBackend: ()      => ipcRenderer.invoke('check-backend'),
-})
